682a44516de8b86dc5552cbd4158b9ac82c48dfd
pingwinno <pingwinno>
2019-02-01 00:12:56 +0200

    Add "video" path

--- src/main/java/com/pingwinno/presentation/SiteLoader.java
+++ src/main/java/com/pingwinno/presentation/SiteLoader.java
@@ -88,6 +88,38 @@ public class SiteLoader {
     }
 
     @GET
+    @Path("/video/{uuid}")
+    @Produces(MediaType.TEXT_HTML)
+    public Response getVideo() throws IOException {
+
+        String baseUrl = uri.getBaseUri().getHost();
+        String[] urlParts = baseUrl.split("\\.");
+        log.trace(baseUrl);
+
+
+        if (!urlParts[0].equals("streamarchive")) {
+            String user = urlParts[0];
+            java.nio.file.Path path = Paths.get("/usr/local/StreamArchiveBackend/" + user + "/index.html");
+            log.debug("streamer {} page loaded", user);
+            return Response.ok().entity(new File(path.toString())).type(Files.probeContentType(path))
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Allow-Methods", "GET, POST")
+                    .header("Access-Control-Max-Age", "1209600").build();
+        } else {
+            java.nio.file.Path path = Paths.get("/usr/local/StreamArchiveBackend/hub/index.html");
+
+            return Response.ok().entity(new File(path.toString())).type(Files.probeContentType(path))
+                    .header("Access-Control-Allow-Origin", "*")
+                    .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+                    .header("Access-Control-Allow-Credentials", "true")
+                    .header("Access-Control-Allow-Methods", "GET, POST")
+                    .header("Access-Control-Max-Age", "1209600").build();
+        }
+    }
+
+    @GET
     @Path("static/{folder}/{file}")
     public Response downloadFile(@PathParam("folder") String folder, @PathParam("file") String fileName) throws IOException {
 
