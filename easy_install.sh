cc06b76d04d9b52bd7d43a97238aa8a8444f6376
pingwinno <pingwinno>
2019-04-06 15:29:40 +0300

    fix folder name

--- easy_install.sh
+++ easy_install.sh
@@ -4,11 +4,11 @@ cd /tmp
 
 ### installing DataBaseApi.jar
 
-FILE=/usr/local/StreamArchiveBackend/DataBaseApi.jar
+FILE=/usr/local/DataBaseApi/DataBaseApi.jar
 if [ -f $FILE ]; then
   echo "tom installed. Updating...."
   git clone https://github.com/pingwinno/DataBaseApi.git
-  cd ./StreamArchiveBackend
+  cd ./DataBaseApi
   mvn package
 else
 ### installing dependencies
@@ -17,10 +17,10 @@ else
   apt upgrade -y
   apt install -y maven git jsvc
   git clone https://github.com/pingwinno/DataBaseApi.git
-  cd ./StreamArchiveBackend
+  cd ./DataBaseApi
   mvn package
   adduser --system --no-create-home --group db-daemon
-  mkdir /usr/local/StreamArchiveBackend/
+  mkdir /usr/local/DataBaseApi/
   mkdir /var/log/db/
   chown archive-daemon /var/log/db/
   chmod u+w /var/log/db/
@@ -29,14 +29,14 @@ else
 fi
 
 
-mv ./target/DataBaseApi.jar /usr/local/StreamArchiveBackend/
+mv ./target/DataBaseApi.jar /usr/local/DataBaseApi/
 mv archive.sh /usr/local/bin/
 chmod +x /usr/local/bin/db.sh
 
 
 cp archive.service /etc/systemd/system/db.service
 cd ../
-rm -rf StreamArchiveBackend
+rm -rf DataBaseApi
 chmod 644 /etc/systemd/system/db.service
 systemctl daemon-reload
 systemctl enable db.service
