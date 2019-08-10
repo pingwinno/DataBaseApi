6e91642e82ced4968398c9d4238a5b2ddd30bc3b
pingwinno <pingwinno>
2019-08-10 07:23:02 +0300

    Added CORS header

--- src/main/java/net/streamarchive/dbapi/StreamerRestApi.java
+++ src/main/java/net/streamarchive/dbapi/StreamerRestApi.java
@@ -1,12 +1,14 @@
 package net.streamarchive.dbapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class StreamerRestApi {
     @Autowired
     StreamerRepository streamerRepository;
--- src/main/java/net/streamarchive/dbapi/StreamsRestApi.java
+++ src/main/java/net/streamarchive/dbapi/StreamsRestApi.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/streams")
 public class StreamsRestApi {
 
